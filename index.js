203adc25ce097e4ae26d0922a0d31150911f41ec
Shyam Mantri <iamshyam1911@gmail.com>
2026-02-26 12:35:34 +0530

    fisrt commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const pug = require("pug");
+
+var app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.get("/home", (req, res) => {
+  const subjects = ["DevOps", "CC", "SPM", "CD", "CNS"];
+  res.render("sample.pug", { sub: subjects });
+});
+
+app.listen(4000, () => {
+  console.log("SERVER RUNNING");
+});
